fbbaea5f0bab28afeb4e6c7033f65cb62e11157e
王渊 <2682220169@qq.com>
2022-08-18 22:13:12 +0800

    第二章

--- C Primer Plus/C Primer Plus.cpp
+++ C Primer Plus/C Primer Plus.cpp
@@ -1,12 +1,205 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+//void two(void)
+//{
+//	printf("two\n");
+//}
+//void one_three(void)
+//{
+//	printf("one\n");
+//	two();
+//	printf("three\n");
+//}
+//int main(void)
+//{
+//	printf("starting now:\n");
+//	one_three();
+//	printf("done!\n");
+//
+//	return 0;
+//}
 
-int main()
-{
-	int a = 10;
-	return 0;
 
-}
+//void s(void)//定义笑
+//{
+//	printf("Smile!");
+//}
+//int main(void)
+//{
+//	s();
+//	s();
+//	s();
+//	printf("\n");
+//	s();
+//	s();
+//	printf("\n");
+//	s();
+//
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//    int toes = 10;
+//    printf("初始值=%d\n", toes);
+//    printf("初始值×2=%d\n", toes * 2);
+//    printf("初始值的平方=%d\n", toes * toes);
+//    return 0;
+//}
+
+
+//void br(void)
+//{
+//	printf("Brazil, Russia");
+//}
+//
+//void ic(void)
+//{
+//	printf("India, China");
+//}
+//
+//int main(void)
+//{
+//	br();
+//	printf(", ");
+//	ic();
+//	printf("\n");
+//	ic();
+//	printf(",\n");
+//	br();
+//	printf("\n");
+//	return 0;
+//}
+
+
+//void jolly(void)
+//{
+//	printf("For he's a jolly good fellow!\n");
+//}
+//void deny(void)
+//{
+//	printf("Which nobody can deny!");
+//}
+//int main(void)
+//{
+//	jolly();
+//	jolly();
+//	jolly();
+//	deny();
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int 姓名 = 18;
+//	int 天数 = 姓名 * 365;
+//	printf("%d %d\n", 姓名, 天数);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	printf("王渊，家住黄土高坡");
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	printf("王渊\n");
+//	printf("王\n渊\n");
+//	printf("王");
+//	printf("渊\n");
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int x, y;
+//	x = 10;
+//	y = 5;
+//	y = x + y;
+//	x = x * y;
+//	printf("%d %d\n", x, y);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int a, b;
+//	a = 5;
+//	b = 2;
+//	b = a;
+//	a = b;
+//	printf("%d %d\n",b,a);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int a, b;
+//	a = 3020;
+//	b = 350;
+//	printf("There were %d words and %d lines.\n", a, b);
+//	return 0;
+//
+//}
+
+
+//int main(void)
+//{
+//	int n, n2, n3;
+//	n = 5;
+//	n2 = n * n;
+//	n3 = n2 * n2;
+//	printf("n = %d, n squared = %d, n cubed = %d\n", n, n2, n3);
+//	return 0;
+//}
+
+
+//void butler(void);
+//int main(void)
+//{
+//	printf("I will summon the butler function.\n");
+//	butler();
+//	printf("Yes.Bring me some tea and writeable DVDs.\n");
+//
+//	return 0;
+//}
+//void butler(void)
+//{
+//	printf("You rang,sir?\n");
+//}
+
+
+//int main()
+//{
+//	int feet, fathoms;
+//	fathoms = 0;
+//	scanf("%d", &fathoms);
+//	feet = 6 * fathoms;
+//	printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+//	return 0;
+//
+//}
+
+
+//int main()
+//{
+//	int doors;
+//	doors = 5;
+//	int dogs;
+//	dogs = 3;
+//	return 0;
+//
+//}
+
 
 //int main()
 //{
@@ -47,3 +240,10 @@ int main()
 //
 //	return 0;
 //}
+
+
+//void butler(void)
+//{
+//	printf("You rang,sir?\n");
+//}
+

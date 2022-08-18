16e5896cd9d6369cf4e144e3d6cb22fb8e3b85c3
王渊 <109222778+wangyuan0225@users.noreply.github.com>
2022-08-18 23:09:03 +0800

    Add files via upload

---
+++ 第3章/ex_03.c
@@ -0,0 +1,12 @@
+/*
+第三章：编程练习 3
+*/
+#include <stdio.h>
+int main(void) {
+    char ch = '\a';
+    printf("%c",ch);
+    /* 输出字符 '\a' 该字符表示警报，但部分系统可能无法发声。*/
+    printf("Starled by the sudden sound, Sally shouted, \n");
+    printf("\"By the Great Pumpkin, what was that!\"\n");
+    return 0;
+}

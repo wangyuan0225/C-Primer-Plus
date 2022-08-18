eee74e88cfdb8ecb45c2961e172df5c6d5a6f01e
王渊 <2682220169@qq.com>
2022-08-18 10:28:57 +0800

    初始导入

---
+++ C Primer Plus/C Primer Plus.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+
+	return 0;
+
+}
+
+//int main()
+//{
+////	printf("hello\n");
+////	printf("%d\n" , sizeof(char));
+////	printf("%d\n", sizeof(short int));
+////	printf("%d\n",sizeof(int));
+////	printf("%d\n", sizeof(long));
+////	printf("%d\n", sizeof(long long));
+////	printf("%d\n", sizeof(float));
+////	printf("%d\n", sizeof(double));
+////	int num1 = 0;
+////	int num2 = 0;
+////	int sum = 0;
+////	scanf("%d%d" , &num1, &num2);
+////	sum = num1 + num2;
+////	printf("%d\n", sum);
+//	//const int n = 10;
+//	//int arr[n] = {0};
+//
+//		return 0;
+//}
+//int main(void)
+//{
+//	int dogs;
+//	printf("How many dogs do you have?\n");
+//	scanf("%d", &dogs);
+//	printf("So you have %d dog(s)!\n", dogs);
+//	return 0;
+//}
+//int main()
+//{
+//	int num = 0;
+//	num = 1;
+//	printf("我是一个简单的");
+//	printf("计算机\n");
+//	printf("我最喜欢的数字是%d，因为它是第一个",num);
+//
+//	return 0;
+//}

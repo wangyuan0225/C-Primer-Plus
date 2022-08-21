b0ed183c58ede3c48ee5eb99c807b3a69cb17f28
王渊 <2682220169@qq.com>
2022-08-21 19:06:32 +0800

    更新

--- C Primer Plus/C Primer Plus.cpp
+++ C Primer Plus/C Primer Plus.cpp
@@ -1,20 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-
 int main(void)
 {
-	float weight;
-	float value;
-	printf("Are you worth your weight in platinum?\n");
-	printf("Let's check it out.\n");
-	printf("Please enter your weight in pounds: ");
-	scanf("%f", &weight);
-	value = 1700.0 * weight * 14.5833;
-	printf("Your weight in platinum is worth $%.2f.\n", value);
-	printf("You are easily worth that!If platinum prices drop,\n");
+	int x = 100;
+	printf("dec=%d;octal=%o;hex=%x\n", x, x, x);
+	printf("dec=%d;octal=%#o;hex=%#x\n", x, x, x);
 
 	return 0;
 }
+
+//int main(void)
+//{
+//	int ten = 10;
+//	int two = 2;
+//	printf("Doint it right:");
+//	printf("%d minus %d is %d\n", ten, 2, ten - two);
+//	printf("Doing it wrong:");
+//	printf("%d minus %d is %d\n", ten);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	float weight;
+//	float value;
+//	printf("Are you worth your weight in platinum?\n");
+//	printf("Let's check it out.\n");
+//	printf("Please enter your weight in pounds: ");
+//	scanf("%f", &weight);
+//	value = 1700.0 * weight * 14.5833;
+//	printf("Your weight in platinum is worth $%.2f.\n", value);
+//	printf("You are easily worth that!If platinum prices drop,\n");
+//
+//	return 0;
+//}
 
 
 //void two(void)

a323ce1649e5d81994b4f19b8ff8547d3128fa37
王渊 <2682220169@qq.com>
2022-08-21 22:31:08 +0800

    更新

--- C Primer Plus/C Primer Plus.cpp
+++ C Primer Plus/C Primer Plus.cpp
@@ -2,12 +2,36 @@
 #include<stdio.h>
 int main(void)
 {
-	int x = 100;
-	printf("dec=%d;octal=%o;hex=%x\n", x, x, x);
-	printf("dec=%d;octal=%#o;hex=%#x\n", x, x, x);
-
+	unsigned int un = 3000000000;
+	short end = 200;
+	long big = 65537;
+	long long verybig = 12345678908642;
+	printf("un=%u and not %d\n", un, un);
+	printf("end=%hd and %d\n", end, end);
+	printf("big=%ld and not %hd\n", big, big);
+	printf("verybig=%lld and not %ld\n", verybig,verybig);
 	return 0;
 }
+
+
+//int main(void)
+//{
+//	int i = 2147483647;
+//	unsigned int j = 4294967295;
+//	printf("%d %d %d\n", i, i + 1, i + 2);
+//	printf("%u %u %u\n", j, j + 1, j + 2);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	int x = 100;
+//	printf("dec=%d;octal=%o;hex=%x\n", x, x, x);
+//	printf("dec=%d;octal=%#o;hex=%#x\n", x, x, x);
+//
+//	return 0;
+//}
 
 //int main(void)
 //{

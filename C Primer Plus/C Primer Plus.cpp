07309563bae2839dace534a5875972cd70bf705c
王渊 <2682220169@qq.com>
2022-08-28 20:37:17 +0800

    更新

--- C Primer Plus/C Primer Plus.cpp
+++ C Primer Plus/C Primer Plus.cpp
@@ -5,14 +5,27 @@
 
 int main(void)
 {
-	float a, b;
-
-	b = 2.0e20 + 1.0;
-	a = b - 2.0e20;
-	printf("%f \n", a);
+	printf("Type int has a size of %u bytes.\n",sizeof(int));
+	printf("Type char has a size of %zd bytes.\n",sizeof(char));
+	printf("Type long has a size of %zd bytes.\n", sizeof(long));
+	printf("Type long long has a size of %zd bytes.\n", sizeof(long long));
+	printf("Type double has a size of %zd bytes.\n", sizeof(double));
+	printf("Type long double has a size of %zd bytes.\n", sizeof(long double));
 
 	return 0;
 }
+
+
+//int main(void)
+//{
+//	float a, b;
+//
+//	b = 2.0e20 + 1.0;
+//	a = b - 2.0e20;
+//	printf("%f \n", a);
+//
+//	return 0;
+//}
 
 
 //int main(void)

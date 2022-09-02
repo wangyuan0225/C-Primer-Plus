200f6f19ea53f6735ab0fd5f0e60d62e3ea2da6d
王渊 <2682220169@qq.com>
2022-09-02 23:49:48 +0800

    更新

--- C Primer Plus/C Primer Plus.cpp
+++ C Primer Plus/C Primer Plus.cpp
@@ -3,19 +3,31 @@
 #include<string.h>
 #include<inttypes.h>
 
-int main(void)
-{
-	float salary;
 
-	printf("\aEnter your desired monthly salary:");
-	printf(" $_______\b\b\b\b\b\b\b");
-	scanf("%f", &salary);
-	printf("\n\t$%.2f a month is $%.2f a year.", salary, 
-		salary * 12.0);
-	printf("\rGee!\n");
 
-	return 0;
-}
+
+//int main(void)
+//{
+//	float salary;
+//	printf("Enter your desired monthly salary:\n");
+//	scanf_s("%f", &salary);
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	float salary;
+//
+//	printf("\aEnter your desired monthly salary:");
+//	printf(" $_______\b\b\b\b\b\b\b");
+//	scanf("%f", &salary);
+//	printf("\n\t$%.2f a month is $%.2f a year.", salary, 
+//		salary * 12.0);
+//	printf("\rGee!\n");
+//
+//	return 0;
+//}
 
 
 //int main(void)
